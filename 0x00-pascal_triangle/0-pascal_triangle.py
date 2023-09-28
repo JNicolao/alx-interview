b9387c6226eb5ac452be8589ab67f392aad9d4aa
JNicolao <bororio96@gmail.com>
2023-09-28 13:44:23 +0300

    Executables

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,40 @@
-#!/usr/bin/env python3
-from typing import List
+#!/usr/bin/python3
+'''Module to find Pascal's Triangle integers'''
 
 
-def pascal_triangle(n: int) -> List[list]:
+def pascal_triangle(n):
     '''
-    Pascal triangle
+    Function to find Pascal's Triangle integers
+
+        Parameters:
+            n (int): The number of row's of Pascal's triangle
+
+        Returns:
+            pascal_triangle (list): Binary string of the sum of a and b
     '''
+    pascal_triangle = list()
+
     if n <= 0:
-        return []
+        return pascal_triangle
 
-    if n == 1:
-        return [[1]]
+    # Add first 1.
+    if n > 0:
+        pascal_triangle.append([1])
 
-    if n == 2:
-        return [[1], [1, 1]]
+    # Add second line.
+    if n > 1:
+        pascal_triangle.append([1, 1])
 
-    triangle = [[1], [1, 1]]
+    for x in range(3, n+1):
+        pascal_triangle.append([0] * x)
 
-    for i in range(2, n):
-        temp = [1, 1]
-        for j in range(0, len(triangle[-1])-1):
-            a = triangle[-1][j]
-            b = triangle[-1][j+1]
-            temp.insert(-1, a + b)
-        triangle.append(temp)
+        # Set first and last 1
+        pascal_triangle[x-1][0] = 1
+        pascal_triangle[x-1][x-1] = 1
 
-    return triangle
+        # Calculate middle numbers
+        for y in range(1, x-1):
+            pascal_triangle[x-1][y] = \
+                pascal_triangle[x-2][y-1] + pascal_triangle[x-2][y]
+
+    return pascal_triangle
