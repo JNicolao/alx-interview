1ac1d7325c88d4e0b6e98c1f8a665794db374644
JNicolao <bororio96@gmail.com>
2023-11-01 14:40:57 +0300

    n-queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -17,6 +17,7 @@ pos = None
 
 def get_input():
     """Retrieves and validates this program's argument.
+
     Returns:
         int: The size of the chessboard.
     """
@@ -38,9 +39,11 @@ def get_input():
 
 def is_attacking(pos0, pos1):
     """Checks if the positions of two queens are in an attacking mode.
+
     Args:
         pos0 (list or tuple): The first queen's position.
         pos1 (list or tuple): The second queen's position.
+
     Returns:
         bool: True if the queens are in an attacking position else False.
     """
@@ -51,8 +54,10 @@ def is_attacking(pos0, pos1):
 
 def group_exists(group):
     """Checks if a group exists in the list of solutions.
+
     Args:
         group (list of integers): A group of possible positions.
+
     Returns:
         bool: True if it exists, otherwise False.
     """
@@ -70,6 +75,7 @@ def group_exists(group):
 
 def build_solution(row, group):
     """Builds a solution for the n queens problem.
+
     Args:
         row (int): The current row in the chessboard.
         group (list of lists of integers): The group of valid positions.
@@ -105,4 +111,3 @@ n = get_input()
 get_solutions()
 for solution in solutions:
     print(solution)
-    
